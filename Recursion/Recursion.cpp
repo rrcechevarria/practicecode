e7f21f92737a1b95f8fb7aec034297161f889707
Rico <56700598+rrcechevarria@users.noreply.github.com>
2019-12-05 23:11:08 -0800

    added fibonacci using memoize solution

--- Recursion/Recursion.cpp
+++ Recursion/Recursion.cpp
@@ -27,28 +27,61 @@ bool checkPrime(int num, int temp)
 		return checkPrime(num, temp - 1);
 }
 
+int fibo(int n, int mem[]) {
+	int result;
+	if (mem[n])
+		return mem[n];
+	if (n == 1 || n == 2)
+		result = 1;
+	else
+		result = fibo(n - 1, mem) + fibo(n - 2, mem);
+	mem[n] = result;
+	return result;
+}
+
 int main()
 {
-	int num, factorial;
-	cout << "Enter a number: ";
-	cin >> num;
+	int num, factorial, choice;
+	bool prime = true;
 
-	factorial = fact(num);
-	cout << "\nFactorial of " << num << " is: " << factorial;
+	cout << "1. Factorial \n"
+		<< "2. Prime \n"
+		<< "3. Fibonacci \n";
+	cin >> choice;
 
-	bool prime = true;	
-	cout << "\nEnter number to check if Prime: ";
-	cin >> num;
-	if (num == 1)
-		cout << endl << num << " is a special number";
-	else
+	switch (choice)
 	{
-		prime = checkPrime(num, num/2);
-		if (prime == true)
-			cout << endl << num << " is prime";
+	case 1: 
+		cout << "Enter a number: ";
+		cin >> num;
+		factorial = fact(num);
+		cout << "\nFactorial of " << num << " is: " << factorial;
+		break;
+	case 2:
+		
+		cout << "\nEnter number to check if Prime: ";
+		cin >> num;
+		if (num == 1)
+			cout << endl << num << " is a special number";
 		else
-			cout << endl << num << " is not prime";
+		{
+			prime = checkPrime(num, num / 2);
+			if (prime == true)
+				cout << endl << num << " is prime";
+			else
+				cout << endl << num << " is not prime";
+		}
+		break;
+	case 3:
+		int  mem[100] = { 0 };
+		cout << "\nEnter a number to find fibonacci equivalent ";
+		cin >> num;
+		cout << "\nFibonacci value: " << fibo(num, mem);
+		break;
 	}
+
+
+	
 	_getch();
 	return 0;
 }
